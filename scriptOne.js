987fe340774381cc8dcbc1d3c4efc54e9294cfdd
scarlettdaprile <skd53@njit.edu>
2024-12-03 10:44:40 -0500

    3 qs

--- scriptOne.js
+++ scriptOne.js
@@ -6,10 +6,20 @@ function greetUser() {
     fName;
 }
 
-
 //Question 2
+function changeText() {
+    document.getElementById('message').innerText = 'Hello, World!';
+}
 
 //Question 3
+function addNumbers() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+  
+    const result = num1 + num2;
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
 
 //Question 4
 
