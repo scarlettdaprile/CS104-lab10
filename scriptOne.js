91323d901caf1a6030635709cba2590cd28cac31
scarlettdaprile <skd53@njit.edu>
2024-12-03 10:39:26 -0500

    question 1

---
+++ scriptOne.js
@@ -0,0 +1,18 @@
+//Question 1
+function greetUser() {
+    let fName = document.getElementbyID('input').value
+
+    document.getElementById('output').innerText = "Hello, " +
+    fName;
+}
+
+
+//Question 2
+
+//Question 3
+
+//Question 4
+
+//Question 5
+
+//Question 6
